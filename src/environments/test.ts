1dad77c1a268183d6cef35b9271ecac8ec353444
barrea_m <barrea_m@etna-alternance.net>
2020-06-01 16:13:42 +0200

    Using a mocking library when testing /verify page

--- src/environments/test.ts
+++ src/environments/test.ts
@@ -1,10 +1,11 @@
 import { IEnv, ITestEnv } from "./base";
 import stagingEnv from "./staging";
+import * as crypto from "crypto";
 
 
 const tests: ITestEnv = {
-	badVerificationToken: "nope",
-	verificationToken: "98c85d44cb9a57028bca78d03d682eb8f7c738eb56d49a4b37b95364d07cb8af3f30561f9e0fa87f0f3440bf41c1a176d5543ed84dcd995d5ec515b5"
+	badVerificationToken: crypto.randomBytes(60).toString("hex"),
+	verificationToken: crypto.randomBytes(60).toString("hex"),
 }
 
 const testEnv: IEnv = Object.assign({}, { tests } as IEnv, stagingEnv);
--- src/tests/homepage.test.ts
+++ src/tests/homepage.test.ts
@@ -1,7 +1,5 @@
 import puppeteer, { Browser, Page } from "puppeteer";
 
-import testEnv from "../environments/test";
-
 describe("Homepage test", () => {
 	let browser: Browser;
 	let page: Page;
@@ -25,43 +23,5 @@ describe("Homepage test", () => {
 		await page.waitForSelector("div.teaser");
 
 		await browser.close();
-	}, 60000);
-});
-
-
-describe("Verify page tests", () => {
-	let browser: Browser;
-	let page: Page;
-
-	beforeAll(async () => {
-		browser = await puppeteer.launch({
-			headless: !!process.env.CI,
-		});
-		page = await browser.newPage();
-	});
-
-	afterAll(async () => {
-		await page.close();
-		await browser.close();
-	});
-
-	test("Verify page with an invalid token", async () => {
-		await page.goto(`http://localhost:3000/verify/${testEnv.tests?.badVerificationToken}`);
-		const x = await page.waitForResponse((r) => r.url().indexOf("/verify") > -1);
-		const errText = await page.evaluate(() => document.querySelector(".teaser h3")?.textContent);
-
-		expect(x.status()).toEqual(400);
-		expect(errText).toBeDefined()
-		expect(errText).toMatch("Oops")
-	}, 60000);
-
-	test("Verify page with an invalid token", async () => {
-		await page.goto(`http://localhost:3000/verify/${testEnv.tests?.verificationToken}`);
-		const x = await page.waitForResponse((r) => r.url().indexOf("/verify") > -1);
-		const errText = await page.evaluate(() => document.querySelector(".teaser h3")?.textContent);
-
-		expect(x.status()).toEqual(200);
-		expect(errText).toBeDefined()
-		expect(errText).toMatch("verified")
 	}, 60000);
 });
